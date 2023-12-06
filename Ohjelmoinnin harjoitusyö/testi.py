d9e04188889d7b3c4e537a79a88f88c36efdb988
weeirdo <128589303+weeirdo@users.noreply.github.com>
2023-12-06 14:32:58 +0200

    Update testi.py
    jygh

--- Ohjelmoinnin harjoitusyö/testi.py
+++ Ohjelmoinnin harjoitusyö/testi.py
@@ -11,16 +11,3 @@ else:
     pienin = l3
 
 print(f"Pienin luku oli {pienin}.")
-
-<<<<<<< HEAD
-vmp täsadäsdäaäsdäas asd sd
-f as
-df 
-sad
-f 
-asdf
-=======
-
-#KIKKELIKAKKELI
-#KADOSAJDOIJA
->>>>>>> 43a25f2b68ca8686d669c67108da6b92528abe76

8fae2a32380f5f6f5910b9bbbfe5811465bf8c07
Lassi Honkanen <131752402+lhhonk@users.noreply.github.com>
2023-12-06 13:57:07 +0200

    Add files via upload

---
+++ Ohjelmoinnin harjoitusyö/testi.py
@@ -0,0 +1,13 @@
+l1 = int(input("Anna luku 1: "))
+l2 = int(input("Anna luku 2: "))
+l3 = int(input("Anna luku 3: "))
+pienin = ""
+
+if l1 < l2 and l1 < l3:
+    pienin = l1
+elif l2 < l3:
+    pienin = l2
+else:
+    pienin = l3
+
+print(f"Pienin luku oli {pienin}.")
